e9fcdc9d3ed5dad50c49fa31007594c62f1a8f9b
carotene512 <karoten512@gmail.com>
2018-05-14 07:08:20 +0900

    mode develpmentでminifyされないことを確認。

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+  // メインとなるJavaScriptファイル（エントリーポイント）
+  entry: `./src/index.js`,
+  mode: 'development',
+
+  // ファイルの出力設定
+  output: {
+    //  出力ファイルのディレクトリ名
+    path: `${__dirname}/dist`,
+    // 出力ファイル名
+    filename: 'main.js'
+  },
+};
+
